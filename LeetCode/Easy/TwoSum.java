de48db758d00826e53ec474275ab44d213f43565
TejaswiMarka <tejaswimarka07@gmail.com>
2025-08-26 23:49:51 +0530

    TwoSum_01

--- LeetCode/Easy/TwoSum.java
+++ LeetCode/Easy/TwoSum.java
@@ -16,16 +16,18 @@
 // Output: [1,2]
 
 class Solution {
-    public:
-        vector<int> twoSum(vector<int>& nums, int target) {
-            int n=nums.size();
-            for(int i=0;i<n-1;i++){
-                for(int j=i+1;j<n;j++){
-                    if(nums[i]+nums[j]==target){
-                        return {i,j};
-                    }
-                }
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                   return new int[] {i,j};
             }
-            return {};
         }
-};
+        return new int[0];
+    }
+}
+
+
+
